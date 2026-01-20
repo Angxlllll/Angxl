ddfbae95eb0637efcbb97011f3e70be49a63ede7
Angxlllll <angxlllll12@gmail.com>
2026-01-20 17:15:40 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,21 +1,20 @@
-import { watchFile, unwatchFile } from 'fs'
 import { fileURLToPath } from 'url'
 
-global.owner = [
-'159606034665538',
-'205819731832938',
-'5714222810',
-'447894206349'
-]
+global.owner = Object.freeze([
+  '159606034665538',
+  '205819731832938',
+  '5714222810',
+  '447894206349'
+])
 
-global.bot = {
-name: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-alias: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-packname: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-author: '𝖠𝗇𝗀𝖾𝗅',
-session: '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍',
-banner: 'https://files.catbox.moe/4k94dp.jpg'
-}
+global.bot = Object.freeze({
+  name: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+  alias: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+  packname: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+  author: '𝖠𝗇𝗀𝖾𝗅',
+  session: '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍',
+  banner: 'https://files.catbox.moe/4k94dp.jpg'
+})
 
 global.namebot = global.bot.name
 global.botname = global.bot.alias
@@ -24,20 +23,12 @@ global.author = global.bot.author
 global.sessions = global.bot.session
 global.banner = global.bot.banner
 
-global.APIs = {
-sky: 'https://api-sky.ultraplus.click',
-may: 'https://mayapi.ooguy.com'
-}
-
-global.APIKeys = {
-sky: process.env.SKY_API_KEY || 'Angxlllll',
-may: process.env.MAY_API_KEY || 'may-684934ab'
-}
-
-const file = fileURLToPath(import.meta.url)
-
-watchFile(file, () => {
-unwatchFile(file)
-console.log(chalk.redBright("config.js actualizado"))
-import(`${file}?update=${Date.now()}`)
+global.APIs = Object.freeze({
+  may: 'https://mayapi.ooguy.com'
 })
+
+global.APIKeys = Object.freeze({
+  may: process.env.MAY_API_KEY ?? 'may-684934ab'
+})
+
+global.__filename = path => fileURLToPath(path)
