3bde0b7848f5f568b0004f305346e58dab5701da
Angxlllll <angxlllll12@gmail.com>
2026-01-20 17:04:21 -0600

    Crear Setnamebot.js

---
+++ plugins/Creador/Setnamebot.js
@@ -0,0 +1,30 @@
+const handler = async (m, { conn, args }) => {
+  const text = args.join(' ').trim()
+
+  if (!text) {
+    return m.reply('*𝖰𝗎𝖾 𝖭𝗈𝗆𝖻𝗋𝖾 𝖣𝖾𝗌𝖾𝖺𝗌 𝖯𝗈𝗇𝖾𝗋𝗆𝖾*')
+  }
+
+  try {
+    conn.sendMessage(m.chat, {
+      react: { text: '✏️', key: m.key }
+    })
+
+    await conn.updateProfileName(text)
+
+    m.reply('*𝖭𝗈𝗆𝖻𝗋𝖾 𝖢𝖺𝗆𝖻𝗂𝖺𝖽𝗈 𝖤𝗑𝗂𝗍𝗈𝗌𝖺𝗆𝖾𝗇𝗍𝖾*')
+  } catch (e) {
+    console.error(e)
+
+    conn.sendMessage(m.chat, {
+      react: { text: '❌', key: m.key }
+    })
+
+    m.reply('*𝖠𝗁 𝖮𝖼𝗎𝗋𝗋𝗂𝖽𝗈 𝖴𝗇 𝖤𝗋𝗋𝗈𝗋 𝖨𝗇𝖾𝗌𝗉𝖾𝗋𝖺𝖽𝗈*')
+  }
+}
+
+handler.command = /^(nombrebot|setnamebot|cambianombre)$/i
+handler.owner = true
+
+export default handler
