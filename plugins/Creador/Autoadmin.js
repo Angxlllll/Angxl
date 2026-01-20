cd411c72e395ed5a8109f8e4a9ea75d632b57423
Angxlllll <angxlllll12@gmail.com>
2026-01-20 16:59:28 -0600

    Crear Autoadmin.js

---
+++ plugins/Creador/Autoadmin.js
@@ -0,0 +1,29 @@
+export default async function (m, { conn, isAdmin }) {
+  if (isAdmin) {
+    return m.reply('*𝖸𝖺 𝖤𝗋𝖾𝗌 𝖠𝖽𝗆𝗂𝗇 𝖩𝖾𝖿𝖾*')
+  }
+
+  conn.sendMessage(m.chat, {
+    react: { text: '⚙️', key: m.key }
+  })
+
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
+
+    conn.sendMessage(m.chat, {
+      react: { text: '⭐', key: m.key }
+    })
+
+    m.reply('*𝖠𝗁𝗈𝗋𝖺 𝖤𝗋𝖾𝗌 𝖠𝖽𝗆𝗂𝗇 𝖩𝖾𝖿𝖾*')
+  } catch {
+    conn.sendMessage(m.chat, {
+      react: { text: '❌', key: m.key }
+    })
+
+    m.reply('*𝖭𝗈 𝗉𝗎𝖽𝗈 𝖽𝖺𝗋𝗍𝖾 𝖺𝖽𝗆𝗂𝗇*')
+  }
+}
+
+export const command = ['autoadmin']
+export const owner = true
+export const group = true
