cccdc4007570b0ef2360c589fdabfcc70a604572
Angxlllll <angxlllll12@gmail.com>
2026-01-20 18:09:32 -0600

    Crear Kickall.js

---
+++ plugins/Creador/Kickall.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn, participants }) => {
+  const botJid = conn.user.jid
+
+  const expulsar = participants
+    .filter(p => p.id !== botJid)
+    .map(p => p.id)
+
+  if (!expulsar.length) {
+    return m.reply('*✅ No hay miembros para expulsar*')
+  }
+
+  try {
+    await conn.groupParticipantsUpdate(m.chat, expulsar, 'remove')
+    await m.reply(`💣 *${expulsar.length} miembros expulsados*`)
+    await conn.groupLeave(m.chat)
+  } catch {
+    await m.reply('*⚠️ WhatsApp bloqueó la acción*')
+  }
+}
+
+handler.help = ['𝖪𝗂𝖼𝗄𝖺𝗅𝗅']
+handler.tags = ['𝖮𝖶𝖭𝖤𝖱']
+handler.command = ['kickall']
+handler.owner = true
+handler.group = true
+export default handler
