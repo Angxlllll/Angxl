d2e8e9c3f4a876756a8a171ab5ab87d8a5ed4319
Angxlllll <angxlllll12@gmail.com>
2026-01-24 14:46:36 -0600

    Actualizar Menu.js

--- plugins/Menus/Menu.js
+++ plugins/Menus/Menu.js
@@ -16,7 +16,6 @@ let handler = async (m, { conn, args }) => {
   }).replace('a. m.', 'A.M').replace('p. m.', 'P.M')
 
   let userId = m.mentionedJid?.[0] || m.sender
-  let user = global.db.data.users[userId]
 
   let _uptime = process.uptime() * 1000
   let uptime = clockString(_uptime)
