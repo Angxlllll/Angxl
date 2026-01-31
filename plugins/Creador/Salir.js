4694eb6d3846b37735ed7ad691069102920f24b8
Angxlllll <angxlllll12@gmail.com>
2026-01-30 18:56:35 -0600

    Agrega carpeta plugins

---
+++ plugins/Creador/Salir.js
@@ -0,0 +1,20 @@
+const handler = async (m, { conn }) => {
+  const mensaje =
+    `𝖥𝗎𝖾 𝖴𝗇 𝖦𝗎𝗌𝗍𝗈 𝖤𝗌𝗍𝖺𝗋 𝖠𝗊𝗎𝗂,\n` +
+    `*${global.namebot}* 𝖲𝖾 𝖣𝖾𝗌𝗉𝗂𝖽𝖾`
+
+  await conn.sendMessage(
+    m.chat,
+    { text: mensaje },
+    { quoted: m }
+  )
+
+  await conn.groupLeave(m.chat)
+}
+
+handler.help = ['𝖲𝖺𝗅𝗂𝗋']
+handler.tags = ['𝖮𝖶𝖭𝖤𝖱']
+handler.command = ["salir"];
+handler.group = true
+handler.owner = true
+export default handler
