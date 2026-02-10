5ce87181eb6e64410b71254c7cb470fac9714966
Angxlllll <angxlllll12@gmail.com>
2026-02-09 21:41:02 -0600

    Actualizar pack.js

--- plugins/pack.js
+++ plugins/pack.js
@@ -2,8 +2,6 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 
-if (!gp.nsfw && m.isGroup) return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
-
 let txt = 'Pack🔥🔥🔥';
 
 let img = 'https://api.delirius.store/nsfw/girls';
