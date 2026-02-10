a24dba61cd2f7f8a8c8257681e319b2fd5088964
Angxlllll <angxlllll12@gmail.com>
2026-02-09 18:53:56 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.opts = yargs(process.argv.slice(2))
   .parse()
 
 const OWNER = Object.freeze([
-  '525911153853@s.whatsapp.net',
+  '5215911153853@s.whatsapp.net',
   '159606034665538@s.whatsapp.net',
   '3264846290995@s.whatsapp.net',
   '205819731832938@s.whatsapp.net'
