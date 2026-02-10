ce0595619c3b1ceb8622b4a5ea973d8106dfbb91
Angxlllll <angxlllll12@gmail.com>
2026-02-09 18:49:29 -0600

    Actualizar Ping.js

--- plugins/Creador/Ping.js
+++ plugins/Creador/Ping.js
@@ -2,17 +2,18 @@ import os from 'os'
 import { performance } from 'perf_hooks'
 
 let handler = async (m, { conn }) => {
+  const start = performance.now()
+
   const used = process.memoryUsage()
+  const totalRam = os.totalmem()
   const freeRam = os.freemem()
 
-  await conn.sendPresenceUpdate('composing', m.chat)
-
-  const start = performance.now()
+  const latency = performance.now() - start
 
   const text = `
 ╭──〔 ${global.namebot} 〕
 │
-│ ⚡ Ping: midiendo...
+│ ⚡ Speed: ${latency.toFixed(2)} ms
 │ 🧠 RAM Used: ${(used.rss / 1024 / 1024).toFixed(1)} MB
 │ 💾 RAM Free: ${(freeRam / 1024 / 1024).toFixed(1)} MB
 │ 💻 Platform: ${process.platform}
@@ -22,17 +23,9 @@ let handler = async (m, { conn }) => {
 `.trim()
 
   await conn.sendMessage(m.chat, { text }, { quoted: m })
-
-  const latency = performance.now() - start
-
-  await conn.sendMessage(
-    m.chat,
-    { text: `⚡ Latencia real aproximada: ${latency.toFixed(0)} ms` },
-    { quoted: m }
-  )
 }
 
-handler.command = ['ping', 'p']
-handler.help = ['ping']
-handler.tags = ['info']
-export default handler
+handler.command = ["ping", "p"];
+handler.help = ["𝖬𝗒𝗅𝗂𝖽"]
+handler.tags = ["𝖮𝖶𝖭𝖤𝖱"]
+export default handler;
